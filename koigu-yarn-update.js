0f8b01f63c0e0dde295862c77b9e6bf1ce9b6795
Anne <anneweeks87@gmail.com>
2026-09-01 22:46:11 -0400

    Koigu update

---
+++ koigu-yarn-update.js
@@ -0,0 +1,371 @@
+(function () {
+  "use strict";
+
+  window.YARN_IMAGE_CATALOG = [
+  {
+    "brand": "Berroco",
+    "name": "Vintage",
+    "image": "https://images4-a.ravelrycache.com/uploads/jmarcella/1032795795/upload_medium2",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/berroco-vintage"
+  },
+  {
+    "brand": "Cascade Yarns",
+    "name": "Cascade 220",
+    "image": "https://images4-a.ravelrycache.com/uploads/keilexandra/1058097573/IMG_4712_medium2.jpeg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/cascade-yarns-cascade-220"
+  },
+  {
+    "brand": "Cascade Yarns",
+    "name": "Cascade 220 Fingering",
+    "image": "https://images4-f.ravelrycache.com/flickr/9/4/7/9473988214/9473988214.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/cascade-yarns-cascade-220-fingering"
+  },
+  {
+    "brand": "Cascade Yarns",
+    "name": "Cascade 220 Sport",
+    "image": "https://images4-g.ravelrycache.com/flickr/5/0/3/5036537643/5036537643.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/cascade-yarns-cascade-220-sport"
+  },
+  {
+    "brand": "Cascade Yarns",
+    "name": "Cascade 220 Grande",
+    "image": "https://images4-a.ravelrycache.com/uploads/meegze/877432247/16640540114256588921080174982036_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/cascade-yarns-cascade-220-grande"
+  },
+  {
+    "brand": "Cascade Yarns",
+    "name": "Pacific",
+    "image": "https://www.cascadeyarns.com/wp-content/uploads/2021/12/Pacific-YarnSkein-151.jpg",
+    "imageSourceUrl": "https://www.cascadeyarns.com/product/pacific"
+  },
+  {
+    "brand": "Kelbourne Woolens",
+    "name": "Germantown",
+    "image": "https://images4-a.ravelrycache.com/uploads/KelbourneWoolens/817457680/Germantown_Square_2in_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/kelbourne-woolens-germantown"
+  },
+  {
+    "brand": "Koigu",
+    "name": "KPPPM",
+    "weight": "Fingering",
+    "yards": 170,
+    "grams": 50,
+    "knitGauge": [
+      28,
+      28
+    ],
+    "needleSize": "US 3 (3.0 mm)",
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "image": "https://images.squarespace-cdn.com/content/v1/591601eec534a5b1eeec38d2/1733790214441-OW9A6HGKTA4X53MWZ1XX/kpppm+michigan.jpg",
+    "imageSourceUrl": "https://www.koigu.com/kpppm/",
+    "sourceUrl": "https://www.koigu.com/kpppm/"
+  },
+  {
+    "brand": "Koigu",
+    "name": "KPM",
+    "weight": "Fingering",
+    "yards": 170,
+    "grams": 50,
+    "knitGauge": [
+      28,
+      28
+    ],
+    "needleSize": "US 3 (3.0 mm)",
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "sourceUrl": "https://www.koigu.com/kpm",
+    "imageSourceUrl": "https://www.koigu.com/kpm"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Kersti",
+    "weight": "DK",
+    "yards": 114,
+    "grams": 50,
+    "knitGauge": [
+      20,
+      20
+    ],
+    "needleSize": "US 6 (4.0 mm)",
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "sourceUrl": "https://www.koigu.com/kersti",
+    "imageSourceUrl": "https://www.koigu.com/kersti"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Jasmine",
+    "weight": "DK",
+    "yards": 242,
+    "grams": 100,
+    "knitGauge": [
+      22,
+      22
+    ],
+    "needleSize": "US 6–7 (3.5–4.0 mm)",
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "sourceUrl": "https://www.koigu.com/jasmine",
+    "imageSourceUrl": "https://www.koigu.com/jasmine"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Lace",
+    "weight": "Lace",
+    "yards": 292,
+    "grams": 50,
+    "knitGauge": [
+      32,
+      46
+    ],
+    "needleSize": "US 0–1 (2.0–2.25 mm)",
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "sourceUrl": "https://www.koigu.com/lace",
+    "imageSourceUrl": "https://www.koigu.com/lace"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Othello",
+    "weight": "Bulky",
+    "yards": 95,
+    "grams": 100,
+    "knitGauge": null,
+    "needleSize": "8 mm",
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "sourceUrl": "https://www.koigu.com/othello",
+    "imageSourceUrl": "https://www.koigu.com/othello"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Chelsea",
+    "weight": "Aran",
+    "yards": 125,
+    "grams": 100,
+    "knitGauge": null,
+    "crochetGauge": null,
+    "fiber": "100% merino wool",
+    "sourceUrl": "https://www.koigu.com/chelsea",
+    "imageSourceUrl": "https://www.koigu.com/chelsea"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Winnie",
+    "weight": "Sport",
+    "yards": 300,
+    "grams": 100,
+    "knitGauge": [
+      23,
+      26
+    ],
+    "needleSize": "US 3–5 (3.25–3.75 mm)",
+    "crochetGauge": null,
+    "fiber": "100% extrafine merino wool (17 micron)",
+    "sourceUrl": "https://www.koigu.com/winnie",
+    "imageSourceUrl": "https://www.koigu.com/winnie"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Sofie",
+    "weight": "Lace",
+    "yards": 238,
+    "grams": 25,
+    "knitGauge": [
+      18,
+      25
+    ],
+    "needleSize": "US 3–8 (3.25–5 mm)",
+    "crochetGauge": null,
+    "fiber": "70% kid mohair / 30% silk",
+    "sourceUrl": "https://www.koigu.com/sofie",
+    "imageSourceUrl": "https://www.koigu.com/sofie"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Andra",
+    "weight": "DK",
+    "yards": 328,
+    "grams": 100,
+    "knitGauge": null,
+    "needleSize": "US 7–9 (4.5–5.5 mm)",
+    "crochetGauge": null,
+    "fiber": "90% extrafine merino / 10% nylon",
+    "sourceUrl": "https://www.koigu.com/andra",
+    "imageSourceUrl": "https://www.koigu.com/andra"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Emmi",
+    "weight": "DK",
+    "yards": 130,
+    "grams": 50,
+    "knitGauge": [
+      22,
+      22
+    ],
+    "needleSize": "US 6–7 (3.5–4.5 mm)",
+    "crochetGauge": null,
+    "fiber": "75% merino / 25% cashmere",
+    "sourceUrl": "https://www.koigu.com/emmi",
+    "imageSourceUrl": "https://www.koigu.com/emmi"
+  },
+  {
+    "brand": "Koigu",
+    "name": "Masham Bouclé",
+    "weight": "Bulky",
+    "yards": 100,
+    "grams": 150,
+    "knitGauge": null,
+    "crochetGauge": null,
+    "fiber": "Masham wool",
+    "sourceUrl": "https://www.koigu.com/blog/2020/5/15/meet-masham-boucl",
+    "imageSourceUrl": "https://www.koigu.com/blog/2020/5/15/meet-masham-boucl"
+  },
+  {
+    "brand": "Lion Brand",
+    "name": "24/7 Cotton",
+    "image": "https://images4-a.ravelrycache.com/uploads/lionbrand/866227358/247cotton_medium2.png",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/lion-brand-24-7-cotton"
+  },
+  {
+    "brand": "Lion Brand",
+    "name": "Heartland",
+    "image": "https://images4-a.ravelrycache.com/uploads/Classy76/871241402/9B1EBAA6-D75C-4480-A16D-C72AF518E644_medium2.jpeg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/lion-brand-heartland"
+  },
+  {
+    "brand": "Lopi",
+    "name": "Léttlopi",
+    "image": "https://images4-a.ravelrycache.com/uploads/Istex/1030335579/9434_Lettlopi_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/istex-lett-lopi"
+  },
+  {
+    "brand": "Malabrigo",
+    "name": "Rios",
+    "image": "https://images4-a.ravelrycache.com/uploads/thegoodwitch/34272098/rios1_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/malabrigo-yarn-rios"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Worsted",
+    "image": "https://www.plymouthyarn.com/sites/default/files/styles/medium/public/yarn/Encore%20Ball%202017.jpg",
+    "imageSourceUrl": "https://www.plymouthyarn.com/yarn/encore-worsted-item-611"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Boucle",
+    "image": "https://images4-f.ravelrycache.com/flickr/3/2/6/3268939745/3268939745.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-boucle"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Chunky",
+    "image": "https://images4-a.ravelrycache.com/uploads/PlymouthYarn/978387434/encore-chunky-9601_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-chunky"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Chunky Colorspun",
+    "image": "https://images4-a.ravelrycache.com/uploads/PlymouthYarn/978390524/encore-chunky-7805_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-chunky-colorspun"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Chunky Tweed",
+    "image": "https://images4-a.ravelrycache.com/uploads/PlymouthYarn/1007719989/encore-tweed-6007_Light_Grey_Heather_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-chunky-tweed"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore DK",
+    "image": "https://images4-a.ravelrycache.com/uploads/PlymouthYarn/35621612/000034_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-dk"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Dynamo",
+    "image": "https://images4-g.ravelrycache.com/flickr/1/1/4/11443037676/11443037676.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-dynamo"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Mega",
+    "image": "https://images4-g.ravelrycache.com/flickr/4/2/5/4255195550/4255195550.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-mega"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Mega Colorspun",
+    "image": "https://www.plymouthyarn.com/sites/default/files/styles/medium/public/yarn/0636_encoremegacolorspun_7159.jpg",
+    "imageSourceUrl": "https://www.plymouthyarn.com/yarn/encore-mega-colorspun-item-636"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Starz",
+    "image": "https://images4-a.ravelrycache.com/uploads/PlymouthYarn/1017772193/encore-starz-G999_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-starz"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Worsted Colorspun",
+    "image": "https://www.plymouthyarn.com/sites/default/files/styles/medium/public/yarn/for%20Web%20Encore%20CS%207203.jpg",
+    "imageSourceUrl": "https://www.plymouthyarn.com/yarn/encore-worsted-colorspun-item-612"
+  },
+  {
+    "brand": "Plymouth Yarn",
+    "name": "Encore Worsted Tweed",
+    "image": "https://images4-a.ravelrycache.com/uploads/PlymouthYarn/1017781521/encore-tweed-W464_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/plymouth-yarn-encore-worsted-tweed"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Comfy Worsted",
+    "image": "https://images4-a.ravelrycache.com/uploads/KPStaff/755360873/upload_medium2",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-comfy"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Simply Alpaca",
+    "image": "https://images4-a.ravelrycache.com/uploads/KPStaff/482084982/5420319_medium2.jpg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-simply-alpaca-aran"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Stroll",
+    "image": "https://images4-a.ravelrycache.com/uploads/BabblingStacey/664682572/upload_medium2",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-stroll-fingering"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Stroll Effects (Hand Painted / Tonal)",
+    "image": "https://images4-a.ravelrycache.com/uploads/LorenWeiss/674257660/upload_medium2",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-stroll-effects"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Bare BFL",
+    "image": "https://di2ponv0v5otw.cloudfront.net/posts/2022/02/10/62056749545a7913fc818e23/m_620568193cda88726f30d717.jpeg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-bare-bfl"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Bare Corriedale Alpaca Blend",
+    "image": "https://d2q9kw5vp0we94.cloudfront.net/i/_/regular/N7758.jpg~w=1000,h=1000,v=1",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-bare-corriedale-alpaca-blend"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "BFL Sport Weight",
+    "image": "https://di2ponv0v5otw.cloudfront.net/posts/2022/02/10/62056749545a7913fc818e23/m_620568193cda88726f30d717.jpeg",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-bfl-sport-weight"
+  },
+  {
+    "brand": "Knit Picks",
+    "name": "Corriedale Alpaca Blend",
+    "image": "https://d2q9kw5vp0we94.cloudfront.net/i/_/regular/N7758.jpg~w=1000,h=1000,v=1",
+    "imageSourceUrl": "https://www.ravelry.com/yarns/library/knit-picks-corriedale-alpaca-blend"
+  }
+];
+}());
